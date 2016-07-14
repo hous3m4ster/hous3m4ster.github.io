90edb6e264104a201383bf1aab08655d0ee9acfb
hous3m4ster <hous3m4ster@protonmail.com>
2016-07-14 23:40:45 +0200

    demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,8 @@
+$.getJSON('test_data.json', function (data) {
+    for(i=0;i<5;i++){
+  text += "<div class=\"col-md-5 jumbotron\"><a href=page.php?id="+i+"><h1>"+data.memes[i].name+"</h1></a><br><img class=\"meme\" src="+data.memes[i].file+" /><br><br><p>"+data.memes[i].description+"</p><br><a href=\""+data.memes[i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>";
+}
+});
 document.write(`<!DOCTYPE html>
   <html>
     <head>
@@ -10,13 +15,10 @@ document.write(`<!DOCTYPE html>
       
     </head>
     <body>
+    <div id=\"header\"><p class=\"hak\">5 memes</p></div><center><div class=\"container-fluid\"><br>
+    `+text+`
     </body>
   </html>
 `);
-document.write("<div id=\"header\"><p class=\"hak\">5 memes</p></div><center><div class=\"container-fluid\"><br>");
-$.getJSON('test_data.json', function (data) {
-    for(i=0;i<5;i++){
-  document.write("<div class=\"col-md-5 jumbotron\"><a href=page.php?id="+i+"><h1>"+data.memes[i].name+"</h1></a><br><img class=\"meme\" src="+data.memes[i].file+" /><br><br><p>"+data.memes[i].description+"</p><br><a href=\""+data.memes[i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>");
-}
-});
+
 
