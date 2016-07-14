fb1248f04ba15d0cdcab6eb0a35be4f89cf5b5c9
hous3m4ster <hous3m4ster@protonmail.com>
2016-07-14 23:44:27 +0200

    demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,4 @@
+var text = "";
 $.getJSON('test_data.json', function (data) {
     for(i=0;i<5;i++){
   text += "<div class=\"col-md-5 jumbotron\"><a href=page.php?id="+i+"><h1>"+data.memes[i].name+"</h1></a><br><img class=\"meme\" src="+data.memes[i].file+" /><br><br><p>"+data.memes[i].description+"</p><br><a href=\""+data.memes[i].file+"\" download><button class=\"btn btn-success\">DOWNLOAD</button></a><button class=\"btn btn-warning\">SHARE</button></div><br>";
@@ -15,8 +16,9 @@ document.write(`<!DOCTYPE html>
       
     </head>
     <body>
-    <div id=\"header\"><p class=\"hak\">5 memes</p></div><center><div class=\"container-fluid\"><br>
+    <div id="header"><p class="hak">5 memes</p></div><center><div class="container-fluid"><br>
     `+text+`
+    </div>
     </body>
   </html>
 `);
